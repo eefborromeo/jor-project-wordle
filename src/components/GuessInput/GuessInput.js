af45acb979629b1b9570094b89a4ce5a18c9deb3
Eloisa Borromeo <27989510+eefborromeo@users.noreply.github.com>
2026-05-03 22:44:50 +0800

    feat: creates GuessInput component

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function GuessInput() {
+	const [guess, setGuess] = React.useState('');
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log({ guess });
+		setGuess('');
+	}
+
+	return (
+		<form onSubmit={e => handleSubmit(e)}>
+			<label htmlFor="guess">Enter Guess:</label>
+			<input
+				id="guess"
+				type="text"
+				name="guess"
+				value={guess}
+				onChange={e => setGuess(e.target.value.toUpperCase())}
+				pattern=".{5}"
+			/>
+		</form>
+	);
+}
+
+export default GuessInput;
