4e8c1e1b463c378a1ae2041bfcee3274ffb07079
Eloisa Borromeo <27989510+eefborromeo@users.noreply.github.com>
2026-05-04 15:20:39 +0800

    feat: enhance Guess component to display individual letters and placeholders

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function Guess({ guesses }) {
+	const placeholder = NUM_OF_GUESSES_ALLOWED - guesses.length;
 	return (
 		<div className="guess-results">
-			{guesses.map(guess => (
-				<p key={crypto.randomUUID()} className="guess">
-					{guess}
-				</p>
-			))}
+			{guesses.length <= NUM_OF_GUESSES_ALLOWED &&
+				guesses.map(guess => {
+					const word = guess.split('');
+					return (
+						<p key={crypto.randomUUID()} className="guess">
+							{word.map(letter => (
+								<span key={crypto.randomUUID()} className="cell">
+									{letter}
+								</span>
+							))}
+						</p>
+					);
+				})}
+			{guesses.length <= NUM_OF_GUESSES_ALLOWED &&
+				range(0, placeholder).map(() => (
+					<p key={crypto.randomUUID()} className="guess">
+						<span className="cell"></span>
+						<span className="cell"></span>
+						<span className="cell"></span>
+						<span className="cell"></span>
+						<span className="cell"></span>
+					</p>
+				))}
 		</div>
 	);
 }
