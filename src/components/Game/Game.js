51966ad70d8ea27e8fd55c046a7e825a8319b45e
Eloisa Borromeo <27989510+eefborromeo@users.noreply.github.com>
2026-05-11 17:16:56 +0800

    feat: add gameStatus state to manage game status in Game component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -12,6 +12,8 @@ console.info({ answer });
 
 function Game() {
 	const [guesses, setGuesses] = React.useState([]);
+	const [gameStatus, setGameStatus] = React.useState([]);
+
 	return (
 		<>
 			<Guess guesses={guesses} answer={answer} />
